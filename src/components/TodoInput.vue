24bc6a1bff9b72fdaa74fc4259df7b57f71c3801
Aamir Khan <aamirkhan@beezlabs.com>
2021-02-18 16:32:09 +0530

    Commit with full flow

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="formInput">
+    <input
+        class="input"
+        placeholder="What needs to be done?"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+    />
+    <i class="downArrow fa fa-angle-right" />
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+import { GET_MY_TODOS } from './TodoPrivateList';
+
+const ADD_TODO = gql`
+mutation ($todo: String!, $isPublic: Boolean!) {
+  insert_todos(objects: {
+    title: $todo,
+    is_public: $isPublic
+  }) {
+    affected_rows
+    returning {
+      id
+      title
+      created_at
+      is_completed
+    }
+  }
+}
+`
+export default {
+  props: ['type'],
+  data() {
+    return {
+      newTodo: '',
+    }
+  },
+  methods: {
+    addTodo() {
+      // insert new todo into db
+      const title = this.newTodo && this.newTodo.trim()
+      const isPublic = this.type === 'public' ? true : false
+      this.$apollo.mutate({
+        mutation: ADD_TODO,
+        variables: {
+          todo: title,
+          isPublic: isPublic
+        },
+        update: (cache, { data: { insert_todos }}) => {
+          try {
+            if (this.type === 'private') {
+              // readQuery will never make a request to our graphql Server
+              const data = cache.readQuery({
+                query: GET_MY_TODOS
+              });
+              const insertedTodo = insert_todos.returning;
+              data.todos.splice(0, 0, insertedTodo[0]);
+              cache.writeQuery({
+                query: GET_MY_TODOS,
+                data
+              })
+            }
+          } catch (e) {
+            console.error(e)
+          }
+          this.newTodo = ''
+        }
+      })
+    },
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,63 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import AuthPlugin from "./plugins/auth";
+import ApolloClient from "apollo-client";
+// import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
+
+Vue.use(AuthPlugin);
+Vue.use(VueApollo);
+
+Vue.config.productionTip = false;
+
+const getHeaders = () => {
+    const headers = {};
+    const token = window.localStorage.getItem('apollo-token');
+    if (token) {
+        headers.authorization = `Bearer ${token}`;
+    }
+    return headers;
+};
+
+// Create an http link:
+// const link = new HttpLink({
+//     uri: 'https://hasura.io/learn/graphql',
+//     fetch,
+//     headers: getHeaders()
+// });
+
+// Create using websockets
+const webSocketsLink = new WebSocketLink({
+    uri: 'wss://hasura.io/learn/graphql',
+    options: {
+        reconnect: true,
+        timeout: 30000,
+        connectionParams: () => {
+            // const authorization = getHeaders
+            // return authorization ? { headers: { authorization } } : {}
+            return { headers: getHeaders() };
+        }
+    }
+})
+
+const client = new ApolloClient({
+    // link: link,
+    link: webSocketsLink,
+    cache: new InMemoryCache({
+        addTypename: true
+    })
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: client,
+})
+
+
+new Vue({
+    router,
+    apolloProvider,
+    render: h => h(App)
+}).$mount("#app");
